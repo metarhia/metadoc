60d5e00dd58043cba09d5b8675047ba173deefc0
Vitaliy Semenchenko <semenchenkovitaliy@gmail.com>
2018-12-18 18:07:34 +0200

    Add initial implementation

---
+++ doc.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const introspector = require('./lib/introspector');
+const generator = require('./lib/generator');
+const types = require('./lib/types');
+
+module.exports = Object.assign(
+  {},
+  introspector,
+  generator,
+  types,
+);
--- metadoc.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Metadoc Stub
-module.exports = {};
